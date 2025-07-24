cd68f8344d15cf7e5e9deb249a01701f526b3b1b
Udomreachea Sambath <udomreacheasambath@gmail.com>
2025-07-24 15:08:24 +0700

    fix: configure semantic-release for protected branches

--- release.config.js
+++ release.config.js
@@ -4,14 +4,7 @@ module.exports = {
     "@semantic-release/commit-analyzer", // reads commit type
     "@semantic-release/release-notes-generator", // generates changelog
     "@semantic-release/changelog", // updates CHANGELOG.md
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md", "package.json"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
     "@semantic-release/github", // creates GitHub Release
   ],
+  repositoryUrl: "https://github.com/reachea/unicode-lang.git",
 };
