105719c30fb49569b3773ddd338b64925866e756
Udomreachea Sambath <udomreacheasambath@gmail.com>
2025-07-24 14:26:06 +0700

    feat: add release workflow.yml

--- release.config.js
+++ release.config.js
@@ -6,5 +6,13 @@ module.exports = {
     "@semantic-release/changelog", // updates CHANGELOG.md
     "@semantic-release/git", // commits changelog and package.json
     "@semantic-release/github", // creates GitHub Release
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
   ],
 };
